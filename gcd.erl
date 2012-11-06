b01dfc0077b0ab563676d2d7e02667a06da2c4ef
Till Theis <theis.till@gmail.com>
2012-11-07 00:22:55 +0100

    file for greatest common divisor algorithm

---
+++ gcd.erl
@@ -0,0 +1,12 @@
+-module(gcd).
+-compile([export_all]).
+
+% die Verzögerungszeit
+% die Terminierungszeit
+% der Startnummer dieses Prozesses (also der wievielte gestartete ggT-Prozess er ist)
+% seine eindeutige Starternummer
+% die Praktikumsgruppennummer
+% die Teamnummer 
+% die benötigten Kontaktdaten für den Namensdienst
+%                             und den Koordinator
+%start()
