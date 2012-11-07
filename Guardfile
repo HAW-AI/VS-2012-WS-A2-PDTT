9df97c4c4b109cf32df873fed15bc1280f2d2de6
Till Theis <theis.till@gmail.com>
2012-11-07 21:23:02 +0100

    guard support for automated compiling and testing

---
+++ Guardfile
@@ -0,0 +1,20 @@
+guard :shell do
+  watch(/(.*).erl/) do |m|
+    compile(m[0]) and run_tests(m[1])
+    puts
+  end
+end
+
+def compile(path)
+  puts "compile #{path}"
+  puts `erlc #{path}`
+  $? == 0
+end
+
+def run_tests(name)
+  puts "#{name}_test.erl"
+  if (File.exists?("#{name}_test.erl"))
+    puts "test #{name}"
+    puts `erl -noshell -eval "eunit:test(#{name}_test, [verbose])" -s init stop`
+  end
+end
--- gcd.erl
+++ gcd.erl
@@ -1,5 +1,6 @@
 -module(gcd).
 -compile([export_all]).
+-include_lib("eunit/include/eunit.hrl").
 
 % die Verzögerungszeit
 % die Terminierungszeit
@@ -9,4 +10,18 @@
 % die Teamnummer 
 % die benötigten Kontaktdaten für den Namensdienst
 %                             und den Koordinator
-%start()
+% start(DelayTime, TerminationTime, ProcessNumber, StarterNumber, GroupNumber, TeamNumberm, NameService, Coordinator) ->
+
+
+% {setneighbors,LeftN,RightN}: die (lokal auf deren Node registrieten und im Namensdienst registrierten) Namen des linken und rechten Nachbarn werden gesetzt.
+% {setpm,MiNeu}: die von diesem Prozess zu berabeitenden Zahl für eine neue Berechnung wird gesetzt.
+% {sendy,Y}: der rekursive Aufruf der ggT Berechnung.
+% {abstimmung,Initiator}: Wahlnachricht für die Terminierung der aktuellen Berechnung; Initiator ist der Initiator dieser Wahl (z.B. Name des ggT-Prozesses).
+% {tellmi,From}: Sendet das aktuelle Mi an From. Kann z.B. vom Koordinator genutzt werden, um bei einem Berechnungsstillstand die Mi-Situation im Ring anzuzeigen.
+% kill: der ggT-Prozess wird beendet.
+
+% Ein ggT-Prozess hat den Namen ?????, wobei ????? eine Zahl ist, die sich wie folgt zusammensetzt: 
+%    <PraktikumsgruppenID><TeamID><Nummer des ggT-Prozess><Nummer des Starters>, 
+% also z.B. ist in der Praktikumsgruppe 4 von dem Team 03 ein zweiter ggT-Prozess von ihrem ersten Starter gestartet worden, so erhält dieser ggT-Prozess den Namen 4321. In der Kommunikation wird nur dieser Name verwendet!  
+process_name(GroupNumber, TeamNumber, ProcessNumber, StarterNumber) ->
+  lists:flatten(io_lib:format("~B~B~B~B", [GroupNumber, TeamNumber, ProcessNumber, StarterNumber])).
