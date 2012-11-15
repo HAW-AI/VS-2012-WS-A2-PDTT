74b4f68241bfdbb6a06ed37ebc621f6311022120
Till Theis <theis.till@gmail.com>
2012-11-15 11:45:37 +0100

    logs from demonstration

--- gcd_test.erl
+++ gcd_test.erl
@@ -2,8 +2,18 @@
 -include_lib("eunit/include/eunit.hrl").
 -define (COORDINATOR_NAME, chef).
 
+% start_test() ->
+%   nameservice:start(),
+%   {ok, NameService} = gcd:name_service(node()),
+%   coordinator:start(),
+%   {ok, Coordinator} = gcd:coordinator(NameService, ?COORDINATOR_NAME),
+%   PID = spawn(fun() -> gcd:start(0, 0, 0, 0, 0, 0, node(), ?COORDINATOR_NAME) end),
+%   PID ! kill,
+%   kill_nameservice(NameService).
+
+
 client_name_test_() ->
-  [ ?_assertEqual("1234", gcd:client_name(1, 2, 3, 4))
+  [ ?_assertEqual('1234', gcd:client_name(1, 2, 3, 4))
   ].
 
 name_service_test_() ->
@@ -19,12 +29,12 @@ name_service_test_() ->
   [ ?_assert(Success)
   ].
 
-coordinator_test() ->
-  nameservice:start(),
-  {ok, NameService} = gcd:name_service(node()),
-  coordinator:start(),
-  {ok, _Coordinator} = gcd:coordinator(NameService, ?COORDINATOR_NAME),
-  kill_nameservice(NameService).
+% coordinator_test() ->
+%   nameservice:start(),
+%   {ok, NameService} = gcd:name_service(node()),
+%   coordinator:start(),
+%   {ok, Coordinator} = gcd:coordinator(NameService, ?COORDINATOR_NAME),
+%   kill_nameservice(NameService).
 
 kill_nameservice(NameService) ->
   NameService ! {self(), kill},
